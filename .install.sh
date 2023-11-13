64fd3654a07c826ae0250f85fda9f29d491a0816
punixcorn <ohyunwoods663@gmail.com>
2023-11-13 17:28:05 +0000

    made some changes

--- .install.sh
+++ .install.sh
@@ -1,23 +1,26 @@
-#!/bin/bash 
+#!/bin/bash
 echo "installing packages"
 
 sudo pacman -S rofi polybar alacritty dunst feh \
-xcb-util-cursor xsettingsd mpc mpd dmenu ncmpcpp \
-networkmanager-dmenu-git light python-gobject \
-xfce4-power-manager maim xclip xorg-xbacklight \
-viewnior conky python-pywal xgd-user-dirs --noconfirm
+	xcb-util-cursor xsettingsd mpc mpd dmenu ncmpcpp \
+	networkmanager-dmenu-git light python-gobject \
+	xfce4-power-manager maim xclip xorg-xbacklight \
+	viewnior conky python-pywal xgd-user-dirs --noconfirm
 
 [ -f /bin/yay ] && yay -S cava || echo "yay not found"
 
-echo "moving files over"
+echo "Moving BSPWM files over
 
-[ -d $(pwd)/others ] && { 
-	mkdir $HOME/.config/networkmanager-dmenu;
-	cp -r $(pwd)/others/config.ini $HOME/.config/networkmanager-dmenu;
-	mkdir $HOME/.local/share/fonts;
-	cp -r $(pwd)/others/fonts/* $HOME/.local/share/fonts;
-	} || echo "others folder not found"
+[ -d $HOME/.config/bspwm/ ] && cp -r $HOME/.config/bspwm $HOME/.config/bspwm-backup && cp -r ../bspwm-dots $HOME/.config/bspwm || cp -r ../bspwm-dots $HOME/.config/bspwm
 
-	[ -f /bin/wal ] &&  wal -i $(pwd)/wallpaper/wallpaper.png
+echo "moving others files over"
+[ -d $(pwd)/others ] && {
+	[ ! -d $HOME/.config/networkmanager-dmenu/ ] && mkdir $HOME/.config/networkmanager-dmenu
+	cp -r $(pwd)/others/config.ini $HOME/.config/networkmanager-dmenu
+	[ ! -d $HOME/.local/share/fonts ] && mkdir $HOME/.local/share/fonts
+	cp -r $(pwd)/others/fonts/* $HOME/.local/share/fonts
+} || echo "others folder not found"
+
+[ -f /bin/wal ] && wal -i $(pwd)/wallpaper/wallpaper.png
 
 exit 0
